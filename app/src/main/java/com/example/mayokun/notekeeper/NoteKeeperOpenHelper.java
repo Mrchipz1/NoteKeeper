812a1dbeb072e72f23915e68edb72c6ecb62f916
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-01-12 17:37:01 +0100

    Added NoteKeeperOpenHelper

---
+++ app/src/main/java/com/example/mayokun/notekeeper/NoteKeeperOpenHelper.java
@@ -0,0 +1,26 @@
+package com.example.mayokun.notekeeper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "NoteKeeper.db";
+    public static final int DATABASE_VERSION = 1;
+    public NoteKeeperOpenHelper(@androidx.annotation.Nullable Context context, @androidx.annotation.Nullable String name,  int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
